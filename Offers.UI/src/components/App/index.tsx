781323ac178f9667d3c8bf4b626a2232aff68e3d
Abhishek Thapliyal <ab.thapliyal@gmail.com>
2020-12-15 06:50:40 +1300

    Renaming file to index so as to simplify imports

---
+++ Offers.UI/src/components/App/index.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import { BulkImportOffer } from '../BulkImportOffer';
+
+import OffersTable from '../OffersTable';
+
+import './App.scss';
+
+function App() {
+
+  return (
+    <Container fluid>
+      <Row>
+        #Header
+      </Row>
+      <Row>
+        <OffersTable />
+      </Row>
+      <Row>
+        #Footer
+      </Row>
+    </Container>
+  );
+}
+
+export default App;
