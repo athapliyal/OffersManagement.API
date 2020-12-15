349479fcef8891de7f91bbf49b1cfdc1086ad185
Abhishek Thapliyal <ab.thapliyal@gmail.com>
2020-12-15 22:23:40 +1300

    Add error validation and change handler to login form

--- Offers.UI/src/components/LoginScreen/LoginForm.tsx
+++ Offers.UI/src/components/LoginScreen/LoginForm.tsx
@@ -1,6 +1,5 @@
-import * as React from "react";
-import { Button } from "react-bootstrap";
-import { useForm } from "react-hook-form";
+import { Alert, Button } from "react-bootstrap";
+import { FieldErrors, useForm } from "react-hook-form";
 
 type FormData = {
     username: string;
@@ -8,26 +7,66 @@ type FormData = {
 };
 
 export const LoginForm: React.FC = () => {
-    const { register, setValue, handleSubmit, errors } = useForm<FormData>();
+  const { register, setValue, handleSubmit, errors } = useForm<FormData>();
 
-    const onSubmit = handleSubmit(({ username, password }) => {
-        console.log(username, password);
-    });
+  const onSubmit = handleSubmit(({ username, password }) => {
+    alert(`${username} ${password}`);
+  });
 
-    return (
-        <form className="login-form" onSubmit={onSubmit}>
-            <div className="form-group">
-                <label htmlFor="username">Username</label>
-                <input className="form-control" name="username" id="username" ref={register} />
-            </div>
+  return (
+    <>
+      <ShowErrors errors={errors} />
 
-            <div className="form-group">
-                <label htmlFor="password">Password</label>
-                <input className="form-control" name="password" id="password" ref={register} />
-            </div>
+      <form className="login-form" onSubmit={onSubmit}>
+        <div className="form-group">
+          <label htmlFor="username">Username</label>
+          <input
+            className="form-control"
+            name="username"
+            id="username"
+            onChange={e => setValue("username", e.target.value)}
+            ref={register({
+              required: "Please enter username",
+            })}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">Password</label>
+          <input
+            className="form-control"
+            name="password"
+            id="password"
+            onChange={e => setValue("password", e.target.value)}
+            ref={register({
+              required: "Please enter password",
+            })}
+          />
+        </div>
 
-            <a className="forgotten-password-link" href="#">Forgotten Password?</a>
-            <Button variant="primary" type="submit" className="login-button">Log in</Button>
-        </form>
-    );
-}
+        <a className="forgotten-password-link" href="#">
+          Forgotten Password?
+        </a>
+        <Button variant="primary" type="submit" className="login-button">
+          Log in
+        </Button>
+      </form>
+    </>
+  );
+};
+
+const ShowErrors: React.FC<FieldErrors> = ({ errors }) => {
+  return (
+    <>
+      {errors?.username && (
+        <Alert variant="danger">
+          <i className="fas fa-exclamation-circle"></i> Please enter username
+        </Alert>
+      )}
+      {errors?.password && (
+        <Alert variant="danger">
+          <i className="fas fa-exclamation-circle"></i> Please enter password
+        </Alert>
+      )}
+    </>
+  );
+};
