2fcd18d1744d51a908712801a8ac47121cbb1731
athapliyal <ab.thapliyal@gmail.com>
2020-12-22 11:54:03 +1300

    Move serviceconstants together

--- Offers.UI/src/services/service-constants.ts
+++ Offers.UI/src/services/service-constants.ts
@@ -1,6 +1,6 @@
 // API CONSTANTS
 // export const BASE_CCC_API_URL = "http://localhost:5000/api";
-export const BASE_CCC_API_URL = "https://localhost:44330/api";
-
 // export const OFFERS_API_BASE_URL = "http://localhost:5000/api/offers";
+
+export const BASE_CCC_API_URL = "https://localhost:44330/api";
 export const OFFERS_API_BASE_URL = "https://localhost:44330/api/offers";
