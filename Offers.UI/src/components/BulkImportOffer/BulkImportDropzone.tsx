fc7d416889c5d2f79a08731a930992590d73e3c0
athapliyal <ab.thapliyal@gmail.com>
2020-12-22 11:52:22 +1300

    Add toastr for bulk import

--- Offers.UI/src/components/BulkImportOffer/BulkImportDropzone.tsx
+++ Offers.UI/src/components/BulkImportOffer/BulkImportDropzone.tsx
@@ -1,21 +1,18 @@
-import { useState, useCallback } from "react";
+import { useCallback } from "react";
 import { useDropzone } from "react-dropzone";
 import { useTranslation } from 'react-i18next';
 
-import { Preloader } from "../Preloader";
-
 import { ToastContainer } from 'react-toastify';
-import { bulkUploadToastSuccess, bulkUploadToastFailure } from '../../notifications/toast-config';
+import { bulkUploadToastStart, bulkUploadToastSuccess, bulkUploadToastFailure } from '../../notifications/toast-config';
 import 'react-toastify/dist/ReactToastify.css';
 
 import { bulkImportService } from '../../services/bulk-import-service';
 
 export const BulkImportDropzone: React.FC = () => {
-  const [uploading, setUploading] = useState<boolean>(false);
   const { t } = useTranslation();
 
   const onDrop = useCallback((acceptedFiles) => {
-    setUploading(true);
+    bulkUploadToastStart();
 
     bulkImportService.uploadBulkImportFile(acceptedFiles[0])
       .then(() => {
@@ -24,7 +21,6 @@ export const BulkImportDropzone: React.FC = () => {
       .catch(() => {
         bulkUploadToastFailure();
       })
-      .finally(() => setUploading(false));
   }, []);
 
   const { getRootProps, getInputProps } = useDropzone({ onDrop, maxFiles: 1 });
@@ -33,13 +29,11 @@ export const BulkImportDropzone: React.FC = () => {
     <>
       <ToastContainer />
       <div className="col-md-8 col-sm-12">
-        { uploading ? <Preloader /> : (
           <div {...getRootProps({ className: "bulk-import__dropzone" })}>
             <input {...getInputProps()} />
             <h1>{t('DragDropCSV')}</h1>
             <p>Or select a file from your computer</p>
           </div>
-        )}
       </div>
     </>
   );
--- Offers.UI/src/notifications/toast-config.ts
+++ Offers.UI/src/notifications/toast-config.ts
@@ -1,5 +1,18 @@
 import { toast } from 'react-toastify';
 
+export const bulkUploadToastStart = () => {
+    toast.success('Offers bulk upload started!', {
+        position: "bottom-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+    });
+}
+
+
 export const bulkUploadToastSuccess = () => {
     toast.success('Offers succesfully uploaded!', {
         position: "bottom-right",
